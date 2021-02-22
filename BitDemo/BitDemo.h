ccec82917543e3e64eb4876177482d7a8a6e6ec4
Глеб Хмарский <hmarsky.gleb@yandex.ru>
2021-02-22 15:39:44 +0300

    Create doc and fill it

---
+++ BitDemo/BitDemo.h
@@ -0,0 +1,33 @@
+
+// BitDemo.h : main header file for the BitDemo application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CBitDemoApp:
+// See BitDemo.cpp for the implementation of this class
+//
+
+class CBitDemoApp : public CWinApp
+{
+public:
+	CBitDemoApp() noexcept;
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+	UINT  m_nAppLook;
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBitDemoApp theApp;
---
+++ BitDemo/BitDemoView.cpp
@@ -0,0 +1,105 @@
+
+// BitDemoView.cpp : implementation of the CBitDemoView class
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "BitDemo.h"
+#endif
+
+#include "BitDemoDoc.h"
+#include "BitDemoView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CBitDemoView
+
+IMPLEMENT_DYNCREATE(CBitDemoView, CView)
+
+BEGIN_MESSAGE_MAP(CBitDemoView, CView)
+END_MESSAGE_MAP()
+
+// CBitDemoView construction/destruction
+
+CBitDemoView::CBitDemoView() noexcept
+{
+	// TODO: add construction code here
+	BITMAP BM;
+	m_Bitmap.LoadBitmap(IDB_BITMAP1);
+	m_Bitmap.GetObject(sizeof(BM), &BM);
+	m_BitmapWidth = BM.bmWidth;
+	m_BitmapHeight = BM.bmHeight;
+
+}
+
+CBitDemoView::~CBitDemoView()
+{
+}
+
+BOOL CBitDemoView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CBitDemoView drawing
+
+void CBitDemoView::OnDraw(CDC* pDC)
+{
+	CBitDemoDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: add draw code for native data here
+	CDC MemDC;
+	RECT ClientRect;
+	// Создадим объект памяти КУ и выберем объект изображения 
+	MemDC.CreateCompatibleDC(NULL);
+	MemDC.SelectObject(&m_Bitmap);
+	// Получим размеры окна представления 
+	GetClientRect(&ClientRect);
+	// Отобразим рисунок с растяжением или сжатием 
+	pDC->StretchBlt
+	(0, 0,              // верхний левый угол приемника
+		ClientRect.right,  // ширина прямоугольника приемника
+		ClientRect.bottom, // высота прямоугольника приемника
+		&MemDC,            // объект КУ источника
+		0, 0,              // верхний левый угол источника
+		m_BitmapWidth,     // ширина прямоугольника источника
+		m_BitmapHeight,    // высота прямоугольника источника
+		SRCCOPY);          // код растровой операции
+
+}
+
+
+// CBitDemoView diagnostics
+
+#ifdef _DEBUG
+void CBitDemoView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CBitDemoView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CBitDemoDoc* CBitDemoView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CBitDemoDoc)));
+	return (CBitDemoDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CBitDemoView message handlers
